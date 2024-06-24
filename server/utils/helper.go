7e14922358df2fdaedc813d4552be6081e6ae35c
Talfaza <charifyahia4@gmail.com>
2024-06-24 14:47:03 +0100

    new: jwt token handling

---
+++ server/utils/helper.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const SecretKey = "secrets"
+
+func GenerateJWT(issuer string) (string, error) {
+	claim := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    issuer,
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // Expires after 24h
+	})
+
+	return claim.SignedString([]byte(SecretKey))
+}
+
+func ClaimParsing(cookie string) (string, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil || !token.Valid {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(*jwt.StandardClaims)
+	if !ok {
+		return "", err 
+	}
+
+	return claims.Issuer, nil
+}
