c0a6e36b8e5b8bc5caba5e8f63d7964be52e5723
Talfaza <charifyahia4@gmail.com>
2024-06-24 11:26:30 +0100

    add: Authentification Controller

--- server/controller/authController.go
+++ server/controller/authController.go
@@ -1,9 +1,10 @@
 package controller
 
 import (
-	"fmt"
+	"log"
 	"regexp"
-
+	"strings"
+	"github.com/Talfaza/bridgehub/database"
 	"github.com/Talfaza/bridgehub/models"
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,9 +19,8 @@ func Register(c *fiber.Ctx) error  {
   var dataUser models.User 
 
   if err := c.BodyParser(&data); err != nil {
-    fmt.Println("Could not convert the data!")
     return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-      "message": "Failed to parse request body",
+      "message": "Could not convert the data!",
       "error":   err.Error(),
     })
   }
@@ -34,6 +34,46 @@ func Register(c *fiber.Ctx) error  {
         "message": "Password must be greater than 8 characters",
     })
   }
+email, ok := data["email"].(string)
+    if !ok {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "message": "Invalid email format",
+        })
+    }
+
+    if !mailCheck(strings.TrimSpace(email)) {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "message": "Invalid Email",
+        })
+    }
+  //checking if email is used 
+
+  database.DB.Where("email=?", strings.TrimSpace(data["email"].(string))).First(&dataUser)
+
+  if dataUser.Id !=0 {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "message": "Email ALready Used",
+      
+        })
+  }
 
 
+
+  user := models.User{
+    
+    Username: data["username"].(string),
+    Email: data["email"].(string),
+  }
+
+
+
+
+  user.HashingPass(data["password"].(string))
+  err:=database.DB.Create(&user)
+  if err != nil {
+    log.Println(err)
+  }
+  return c.JSON(fiber.Map{
+    "message": "Registration successful",
+  })
 }
