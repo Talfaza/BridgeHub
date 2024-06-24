ea9b02506f48204a35bb4dd9ede2347229a098e7
Talfaza <charifyahia4@gmail.com>
2024-06-24 13:54:02 +0100

    new: register router

---
+++ server/routes/route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Talfaza/bridgehub/controller"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RouteSetup(app *fiber.App)  {
+
+  app.Post("/api/register",controller.Register)
+
+
+  
+}
