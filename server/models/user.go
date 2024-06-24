fe64f5670a8f31782a560378a0e588a4bf4e366a
Talfaza <charifyahia4@gmail.com>
2024-06-24 11:26:50 +0100

    add: Password Hashing

--- server/models/user.go
+++ server/models/user.go
@@ -1,8 +1,20 @@
 package models
-
+import(
+  "golang.org/x/crypto/bcrypt"
+)
 type User struct {
   Id       uint   `json:"id"`
   Username string `json:"username"` 
   Email    string `json:"email"`
   Password []byte `json:"-"`
 }
+
+func (user *User) HashingPass(password string) ([]byte, error) {
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return nil, err
+	}
+	user.Password = hashedPass
+	return user.Password, nil
+}
+
