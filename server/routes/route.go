6abe69ed6ef084476aa49b2a49d6979b03b6ce39
Talfaza <charifyahia4@gmail.com>
2024-06-24 21:00:08 +0100

    add: ssh router

--- server/routes/route.go
+++ server/routes/route.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/Talfaza/bridgehub/controller"
 	"github.com/gofiber/fiber/v2"
+  "github.com/Talfaza/bridgehub/middleware"
 )
 
 func RouteSetup(app *fiber.App)  {
@@ -10,7 +11,7 @@ func RouteSetup(app *fiber.App)  {
   app.Post("/api/register",controller.Register)
   app.Post("api/login",controller.Login)
   //user will not acces any route under this line if he is not authentificated
-  // app.Use(middleware.IsAuth) 
-
+  app.Use(middleware.IsAuth) 
+  app.Post("/api/execute", controller.ExecuteCommand)
   
 }
