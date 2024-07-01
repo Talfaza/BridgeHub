c1bae57ba0cf044c63f70683b575a1c2f5a0c921
Talfaza <charifyahia4@gmail.com>
2024-07-02 00:05:17 +0100

    add: navbar (create component later)

--- client/src/components/global/Dashboard.tsx
+++ client/src/components/global/Dashboard.tsx
@@ -1,23 +1,35 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../ui/button';
 
 const Dashboard: React.FC = () => {
     const token = localStorage.getItem('token');
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!token) {
-            window.location.href = '/login'; 
+            navigate('/login');
         }
-    }, [token]);
+    }, [token, navigate]);
 
     const handleLogout = () => {
-        localStorage.removeItem('token'); // Remove token from localStorage
-        window.location.href = '/login';     };
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
 
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>Welcome to the dashboard!</p>
-            <button onClick={handleLogout}>Logout</button>
+            <nav className="sticky top-0 z-10 bg-white shadow-md">
+                <div className="max-w-19xl mx-auto px-4"> 
+                    <div className="flex items-center justify-between h-16">
+                        <span className="text-2xl text-orange-600 font-semibold">BridgeHub</span>
+                       
+                        <Button onClick={handleLogout}>
+                            Logout
+                        </Button>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
