fda0cfbfb8e492c0e34a7d6686a73456b578af90
Talfaza <charifyahia4@gmail.com>
2024-07-10 23:39:22 +0100

    add: Delete server API and some ui changes

--- client/src/components/ServerCard.tsx
+++ client/src/components/ServerCard.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import { TrashIcon, ArrowLeftIcon } from "@radix-ui/react-icons";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "./ui/button";
 
 export function ServerCard() {
   const [servers, setServers] = useState([]);
@@ -27,10 +28,26 @@ export function ServerCard() {
     fetchServers();
   }, []);
 
+  const deleteServer = async (serverID) => {
+    try {
+      const response = await axios.delete(`http://localhost:3000/api/deleteserver/${serverID}`, {
+        withCredentials: true,
+      });
+
+      if (response.status === 200) {
+        setServers(servers.filter(server => server.id !== serverID));
+      } else {
+        console.error("Failed to delete server:", response.data.message);
+      }
+    } catch (error) {
+      console.error("Error deleting server:", error);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-background">
       <div className="p-8 flex flex-col space-y-4">
-          <Link to="/dashboard" className="text-white py-2 px-4   flex ">
+        <Link to="/dashboard" className="text-white py-2 px-4 flex">
           <ArrowLeftIcon className="h-5 w-5 mr-2" />
           Back
         </Link>
@@ -43,6 +60,10 @@ export function ServerCard() {
                 <CardDescription>{server.hostname}@{server.ipaddress}</CardDescription>
               </CardHeader>
               <CardContent>
+                <Button variant="destructive" onClick={() => deleteServer(server.id)} className="flex items-center">
+                  <TrashIcon className="h-5 w-5 mr-2" />
+                  Delete Server
+                </Button>
               </CardContent>
             </Card>
           ))}
