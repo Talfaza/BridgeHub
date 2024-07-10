6965a5e96736931dba8f2b43116994c87d948101
Talfaza <charifyahia4@gmail.com>
2024-07-10 21:49:01 +0100

    add: routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import { Dashboard } from "./components/Dashboard";
 import { Authentification } from './components/Authentification';
+import { ServerCard } from './components/ServerCard'; 
 function App() {
   return (
    <Router>
       <div>
         <Routes>
+
           <Route path="/" element={<Authentification/>} />
+          <Route path="/servers" element={<ServerCard/>} />
           <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
       </div>
