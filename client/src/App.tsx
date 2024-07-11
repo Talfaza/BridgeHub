e08c4d6ad9882f8b39327163a291829d106830c0
Talfaza <charifyahia4@gmail.com>
2024-07-11 10:38:18 +0100

    add: Routing for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import { Dashboard } from "./components/Dashboard";
 import { Authentification } from './components/Authentification';
 import { ServerCard } from './components/ServerCard'; 
+import { CommandsManage } from './components/CommandsManage';
 function App() {
   return (
    <Router>
@@ -9,6 +10,7 @@ function App() {
         <Routes>
 
           <Route path="/" element={<Authentification/>} />
+          <Route path="/tests" element={<CommandsManage/>} />
           <Route path="/servers" element={<ServerCard/>} />
           <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
