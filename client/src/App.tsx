12f94f36f4ec9179f2e24dbf8b208888c7f39943
Talfaza <charifyahia4@gmail.com>
2024-07-11 16:25:32 +0100

    add: more testing routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Authentification } from './components/Authentification';
 import { ServerCard } from './components/ServerCard'; 
 import { CommandsManage } from './components/CommandsManage';
+import { DialogMkdir } from './components/commands/Mkdir'; 
 function App() {
   return (
    <Router>
@@ -11,6 +12,7 @@ function App() {
 
           <Route path="/" element={<Authentification/>} />
           <Route path="/tests" element={<CommandsManage/>} />
+          <Route path="/tests/mkdir" element={<DialogMkdir/>} />
           <Route path="/servers" element={<ServerCard/>} />
           <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
